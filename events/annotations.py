104f67491c26a1a337dbc0dd7f8fa4f472bf1f87
David Diaz <David.Diaz@infoteam.de>
2026-04-17 17:23:33 +0200

    eod commit

--- events/annotations.py
+++ events/annotations.py
@@ -1,66 +1,33 @@
-from functools import wraps
-from typing import Any, Callable, ParamSpec, TypeVar
+from typing import Any, Callable
+from .system import get_event_system
 
-from events.system import get_event_system
-
-
-P = ParamSpec("P")
-R = TypeVar("R")
-T = TypeVar("T", bound=type[Any])
-
-def event_model(*, event_type: str, target: object | None = None):
-    def decorator(cls: T) -> T:
-
+#region class decorators
+def event_model(*, event_type: str) -> Callable[[type], type]:
+    # marks model as event
+    def event_decorator(cls: type) -> type:
         original_init = cls.__init__
-
-        @wraps(original_init)
-        def __init__(self: Any, *args: Any, **kwargs: Any):
-            original_init(self, *args, **kwargs)
-            self.type = event_type
-            self.target = target
+        
+        def __init__(self, *args, **kwargs):  #type: ignore
+            original_init(self, *args, **kwargs) #type: ignore
+            self.event_type = event_type
 
         cls.__init__ = __init__
-        cls._is_event_model = True
         return cls
-    return decorator
+    return event_decorator
 
-def event_listener(*, event_type: str, target: object | None = None) -> Callable[[Callable[P, Any]], Callable[P, Any]]:
-    event_system = get_event_system()
+def event_listener(cls: type) -> type:
+    pass
 
-    def decorator(func: Callable[P, Any]) -> Callable[P, Any]:
-        @wraps(func)
-        def callback(*args: P.args, **kwargs: P.kwargs) -> None:
-            func(*args, **kwargs)  # ignore return value
 
-        event_system.subscribe(event_type, callback, target)
 
-        setattr(func, "_event_listener", {
-            "callback": callback,
-            "event_type": event_type,
-            "target": target,
-        })
-        return func
-
-    return decorator
-
-def event_source(*, event_type: str, target: object | None = None):
-    event_system = get_event_system()
-    def decorator(func: Callable[..., R]) -> Callable[..., R]:
-        @wraps(func)
-        def wrapper(*args: Any, **kwargs: Any) -> R:
-            result = func(*args, **kwargs)
-            if not hasattr(type(result), "_is_event_model"):
-                raise ValueError(f"Return value of function '{func.__name__}' must be an event model")
-            event_system.fire(result)
-            return result
+#region function decorators
+def event_source(*, event_type: str, target_id: str | None = None) -> Callable[..., object]:
+    # marks function as event source
+    def event_source_decorator(func: Callable[..., object]) -> Callable[..., object]:
+        def wrapper(self: object, *args: Any, **kwargs: Any) -> None:
+            result: object = func(self, *args, **kwargs)
+            get_event_system().emit(event_type, result, target_id)
         return wrapper
+    return event_source_decorator
 
-    return decorator
 
-def unsubscribe_listener(func: Callable[..., Any]):
-    if not hasattr(func, "_event_listener"):
-        raise ValueError(f"Function '{func.__name__}' is not an event listener")
-    event_listener_info = getattr(func, "_event_listener")
-    event_system = get_event_system()
-    event_system.unsubscribe(event_listener_info["event_type"], event_listener_info["callback"])
-    delattr(func, "_event_listener")
--- events/system.py
+++ events/system.py
@@ -1,71 +1,32 @@
-from typing import Any, Callable
 
-event_system_instance: '_EventSystem | None' = None
+_event_system: 'EventSystem | None' = None
 
-def init():
-    global event_system_instance
-    if event_system_instance is not None:
-        return
-    event_system_instance = _EventSystem()
+def get_event_system() -> 'EventSystem':
+    global _event_system
+    if _event_system is None:
+        _event_system = EventSystem()
+    return _event_system
 
-def get_event_system() -> '_EventSystem':
-    if event_system_instance is None:
-        raise RuntimeError("Event system not initialized. Call 'init()' before using the event system.")
-    return event_system_instance
+class EventSystem:
 
 
-class _Subscriptions:
+    def register_event_source(self, source: object) -> None:
+        pass
 
-    def __init__(self, callback: Callable[..., None], target: Any):
-        self.callback = callback
-        self.target = target
+    def unregister_event_source(self, source: object) -> None:
+        pass
 
-class _EventSystem:
+    def emit(self, event_type: str, event_model: object, target_id: str | None = None) -> None:
+        pass
+
+    def subscribe(self, event_type: str, event_listener: object) -> None:
+        pass
+
+    def unsubscribe(self, event_type: str, event_listener: object) -> None:
+        pass
 
 
-    def __init__(self):
-        self._subscriptions: dict[str, list[_Subscriptions]] = {}
+class _Subscriber:
 
-    def subscribe(self, event_type: str, callback: Callable[..., None], target: Any = None):
-        if event_type not in self._subscriptions:
-            self._subscriptions[event_type] = []
-        elif self._is_already_subscribed(event_type, callback, target):
-            return
-        self._subscriptions[event_type].append(_Subscriptions(callback, target))
-
-    def unsubscribe(self, event_type: str, callback: Callable[..., None]):
-        if not event_type in self._subscriptions:
-            raise ValueError(f"No subscriptions for event type '{event_type}'")
-        subscribers = self._subscriptions[event_type]
-
-        subscriber_to_remove: _Subscriptions | None = None
-        for subscription in subscribers:
-            if subscription.callback == callback:
-                subscriber_to_remove = subscription
-                return
-            
-        if subscriber_to_remove is not None:
-            subscribers.remove(subscriber_to_remove)
-            return
-        
-        raise ValueError(f"No subscription for event type '{event_type}' with callback '{callback}'")
-    
-    def fire(self, event: object):
-        if not hasattr(type(event), "_is_event_model"):
-            raise ValueError(f"Event must be an event model")
-        event_type = getattr(event, "type", None)
-        if event_type is None:
-            raise ValueError(f"Event model must have a 'type' attribute")
-        if event_type not in self._subscriptions:
-            return
-        for subscription in self._subscriptions[event_type]:
-            if subscription.target is None or subscription.target == getattr(event, "target", None):
-                subscription.callback(event)
-
-    def _is_already_subscribed(self, event_type: str, callback: Callable[..., None], target: Any) -> bool:
-        if event_type not in self._subscriptions:
-            return False
-        for subscription in self._subscriptions[event_type]:
-            if subscription.callback == callback and subscription.target == target:
-                return True
-        return False
+    def __init__(self, event_listener: object):
+        pass
